b62a55fa4de042046d7ae6c57bd73060d3510a96
Daniel <komolafedanielolaoluwa@gmail.com>
2024-03-23 18:50:46 -0700

    .

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Creates a user and a database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Creates a user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant priviledges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
+
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
