7c69b5c753ed1b515624179e3dc244e8b7de549b
Daniel <komolafedanielolaoluwa@gmail.com>
2024-03-23 19:05:56 -0700

    added the files to prepare a sql server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Creates a user and a database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Creates a user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant priviledges
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
