ce74c2f6a472577d7e22a6d951530eb23f55878e
Daniel <komolafedanielolaoluwa@gmail.com>
2024-03-25 17:40:02 -0700

    finished the sixth task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,9 +15,18 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        all_cls = [BaseModel, User, Place, State, City, Amenity, Review]
+        cls_obj = {}
+        if cls in all_cls:
+            for k, v in FileStorage.__objects.items():
+                val = v.to_dict()
+                if cls == eval(val['__class__']):
+                    cls_obj[k] = v
+        return cls_obj
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -25,15 +41,19 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+        """Deletes objects from the storage dictionary"""
+        if obj is None:
+            return
+        # Retrieve key
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        if key in FileStorage.__objects.keys():
+            del self.all()[key]
+            self.save()
+            return
+
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
